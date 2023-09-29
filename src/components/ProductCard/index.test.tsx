d61cc6dc39c6db658a70f4849cd229ee45249041
TousifProvat <tousifprovat15@gmail.com>
2023-09-29 17:10:49 +0600

    fix: rename product card test to index.test.tsx

---
+++ src/components/ProductCard/index.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+
+import ProductCard from ".";
+
+describe("<ProductCard/>", () => {
+  it("should render the product name, description, price and stock", () => {
+    render(
+      <ProductCard
+        name="Walton TV"
+        description="4k tv"
+        price={1000}
+        stock={20}
+      />,
+    );
+
+    expect(screen.getAllByText(/Walton TV/)).toHaveLength(2);
+    expect(screen.getByText(/4k tv/)).toBeInTheDocument();
+    expect(screen.getByText(/1000\$/, { exact: true })).toBeInTheDocument();
+    expect(screen.getByText(/20 qty/, { exact: true })).toBeInTheDocument();
+  });
+});
