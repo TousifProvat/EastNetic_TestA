eb8a099471c8f1bc3ea35453920d334182211bec
TousifProvat <tousifprovat15@gmail.com>
2023-09-29 17:06:36 +0600

    chore: lint-staged and prettier

--- src/components/ProductCard/ProductCard.test.tsx
+++ src/components/ProductCard/ProductCard.test.tsx
@@ -1,18 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import ProductCard from '.';
+import { render, screen } from "@testing-library/react";
 
-describe('<ProductCard/>', () => {
-  it('should render the product name, description, price and stock', () => {
+import ProductCard from ".";
+
+describe("<ProductCard/>", () => {
+  it("should render the product name, description, price and stock", () => {
     render(
       <ProductCard
-        name='Walton TV'
-        description='4k tv'
+        name="Walton TV"
+        description="4k tv"
         price={1000}
         stock={20}
-      />
+      />,
     );
 
-    expect(screen.getAllByText(/Walton TV/).length).toBe(2);
+    expect(screen.getAllByText(/Walton TV/)).toHaveLength(2);
     expect(screen.getByText(/4k tv/)).toBeInTheDocument();
     expect(screen.getByText(/1000\$/, { exact: true })).toBeInTheDocument();
     expect(screen.getByText(/20 qty/, { exact: true })).toBeInTheDocument();
